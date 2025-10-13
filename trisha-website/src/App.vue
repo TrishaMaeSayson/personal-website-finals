1b08947d5f98d8f7f3249f48cd5ea45a6ef0de0f
TrishaMaeSayson <tvsayson2@student.apc.edu.ph>
2025-10-13 21:03:12 +0800

    revised sidebar.vue and css

--- trisha-website/src/App.vue
+++ trisha-website/src/App.vue
@@ -1,26 +1,20 @@
 <template>
   <div class="container-fluid">
-    <div class="row g-0">
+    <div class="row g-0 flex-nowrap">
       <!-- Sidebar -->
-      <div class="col-md-2">
+      <div class="col-12 col-md-2 sidebar-col">
         <Sidebar />
       </div>
 
       <!-- Main content -->
-      <div class="col-md-10 p-4">
-        <div class="row">
-          <div class="col-2"></div>
-
-          <div class="col-8">
-            <Home />
-            <About />
-            <Education />
-            <Achievements />
-            <Contact />
-            <Resources />
-          </div>
-
-          <div class="col-2"></div>
+      <div class="col-12 col-md-10 main-content">
+        <div class="p-4">
+          <Home />
+          <About />
+          <Education />
+          <Achievements />
+          <Contact />
+          <Resources />
         </div>
       </div>
     </div>
--- trisha-website/src/components/Sidebar.vue
+++ trisha-website/src/components/Sidebar.vue
@@ -1,37 +1,36 @@
 <template>
-  <div>
-    <!-- Mobile toggle button -->
-    <button 
-      class="btn btn-primary d-md-none m-3"
+  <nav class="sidebar navbar navbar-expand-lg navbar-dark bg-dark flex-lg-column align-items-center p-3">
+    <!-- Brand / Profile -->
+    <a class="navbar-brand text-center w-100 mb-2" href="#home">
+      <img src="/images/selfie.jpg" class="profile-pic mb-2" alt="Profile" />
+      <h1>TreseMe</h1>
+    </a>
+
+    <!-- Hamburger Button -->
+    <button
+      class="navbar-toggler"
       type="button"
-      data-bs-toggle="offcanvas"
-      data-bs-target="#mobileSidebar"
-      aria-controls="mobileSidebar"
+      data-bs-toggle="collapse"
+      data-bs-target="#sidebarNav"
+      aria-controls="sidebarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
     >
-      <i class="fas fa-bars"></i>
+      <span class="navbar-toggler-icon"></span>
     </button>
 
-    <!-- Sidebar (desktop & offcanvas) -->
-    <div 
-      class="offcanvas-md offcanvas-start show position-static border-end" 
-      tabindex="-1" 
-      id="mobileSidebar"
-    >
-      <div class="offcanvas-body profile-section text-center w-100 py-4">
-        <img src="/images/selfie.jpg" class="profile-pic mb-2" alt="Profile" />
-        <h1>TreseMe</h1>
-
-        <ul class="nav flex-column mt-4">
-          <li class="nav-item"><a href="#home" class="nav-link"><i class="fas fa-home"></i> Home</a></li>
-          <li class="nav-item"><a href="#about" class="nav-link"><i class="fas fa-user"></i> About</a></li>
-          <li class="nav-item"><a href="#education" class="nav-link"><i class="fas fa-graduation-cap"></i> Education</a></li>
-          <li class="nav-item"><a href="#achievement" class="nav-link"><i class="fas fa-award"></i> Achievements</a></li>
-          <li class="nav-item"><a href="#contact" class="nav-link"><i class="fas fa-envelope"></i> Contact</a></li>
-          <li class="nav-item"><a href="#resources" class="nav-link"><i class="fas fa-book"></i> Resources</a></li>
-        </ul>
-      </div>
+    <!-- Collapsible Menu -->
+    <div class="collapse navbar-collapse flex-column mt-3 w-100" id="sidebarNav">
+      <ul class="navbar-nav w-100 text-center">
+        <li class="nav-item"><a href="#home" class="nav-link"><i class="fas fa-home"></i> Home</a></li>
+        <li class="nav-item"><a href="#about" class="nav-link"><i class="fas fa-user"></i> About</a></li>
+        <li class="nav-item"><a href="#education" class="nav-link"><i class="fas fa-graduation-cap"></i> Education</a></li>
+        <li class="nav-item"><a href="#achievement" class="nav-link"><i class="fas fa-award"></i> Achievements</a></li>
+        <li class="nav-item"><a href="#contact" class="nav-link"><i class="fas fa-envelope"></i> Contact</a></li>
+        <li class="nav-item"><a href="#resources" class="nav-link"><i class="fas fa-book"></i> Resources</a></li>
+      </ul>
     </div>
-  </div>
+  </nav>
 </template>
 
 <script>
