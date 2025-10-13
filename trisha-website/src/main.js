062fed0b27bf04eb878439f6a6b8639ea6c69e58
TrishaMaeSayson <tvsayson2@student.apc.edu.ph>
2025-10-13 14:01:51 +0000

    revised path for css

--- trisha-website/src/main.js
+++ trisha-website/src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import './assets/style.css'  // link to your separate CSS file
+import '/style.css'  // link to your separate CSS file
 
 createApp(App).mount('#app')
