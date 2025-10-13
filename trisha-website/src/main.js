cac81a6ec9ffc628f308fcc78ea9f0fb5f320d35
TrishaMaeSayson <tvsayson2@student.apc.edu.ph>
2025-10-13 13:52:49 +0000

    added imports in main

--- trisha-website/src/main.js
+++ trisha-website/src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import './assets/style.css'  // link to your separate CSS file
 
 createApp(App).mount('#app')
