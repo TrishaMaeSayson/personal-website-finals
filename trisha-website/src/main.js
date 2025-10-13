929e807fde86de4586a8d0b24ab3c8a7c2370e48
TrishaMaeSayson <tvsayson2@student.apc.edu.ph>
2025-10-13 14:04:08 +0000

    removed import css

--- trisha-website/src/main.js
+++ trisha-website/src/main.js
@@ -2,6 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import '/style.css'  // link to your separate CSS file
 
 createApp(App).mount('#app')
